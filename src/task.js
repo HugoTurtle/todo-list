e784f3f17c79f8a3b66bb96f895d9a7dbc2f89ee
Hugo Perez <hugopereztm12309@gmail.com>
2025-05-15 18:53:19 -0400

    Add a method to modify all task parameters

--- src/task.js
+++ src/task.js
@@ -6,4 +6,10 @@ export class Task {
         this.dueDate = dueDate;
         this.completed = false;
     }
+    modifyTask(title = this.title, description = this.description, priorityLevel = this.priorityLevel, dueDate = this.dueDate) {
+        this.title = title;
+        this.description = description;
+        this.priorityLevel = priorityLevel;
+        this.dueDate = dueDate;
+    }
 }
