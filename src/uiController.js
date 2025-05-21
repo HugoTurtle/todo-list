71e3c7123a0dea8e934f89b04a705b2af5b96afb
Hugo Perez <hugopereztm12309@gmail.com>
2025-05-21 16:18:27 -0400

    Add renderTask function to display tasks in DOM

--- src/uiController.js
+++ src/uiController.js
@@ -1,6 +1,23 @@
 import { TaskManager } from "./taskManager";
 
 export function renderTask(tasks) {
-    const main = document.querySelector('main');
-    main.innerHTML = '';
+    const taskContainer = document.getElementById('task-container');
+
+    for(const task of tasks) {
+        const taskItem = document.createElement('div');
+        taskItem.classList.add('task-item');
+
+        const title = document.createElement('p');
+        title.textContent = task.title;
+
+        const description = document.createElement('p');
+        description.textContent = task.description;
+
+        const dueDate = document.createElement('p');
+        dueDate.textContent = task.dueDate;
+
+        taskItem.append(title, description, dueDate);
+
+        taskContainer.append(taskItem);
+    }
 }
