5bdd6a3970133178143b0283744d774127b59990
Hugo Perez <hugopereztm12309@gmail.com>
2025-05-21 17:39:03 -0400

    Add a method to add projects to an array

--- src/projectManager.js
+++ src/projectManager.js
@@ -2,4 +2,10 @@ export class ProjectManager {
     constructor() {
         this.projects = [];
     }
+
+    addProject(name) {
+        const project = {name, id};
+        this.projects.push(project);
+        return project;
+    }
 }
