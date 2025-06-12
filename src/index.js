7d750b3607a2803fb6eae49684841b1ba2b50f4a
Hugo Perez <hugopereztm12309@gmail.com>
2025-06-12 17:53:45 -0400

    Add task button logic

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import "./styles.css";
 import { ProjectManager } from "./projectManager";
 import { renderProjects } from "./uiController";
+import { addTaskButton } from "./uiController";
+import { TaskManager } from "./taskManager";
+import { renderTask } from "./uiController";
 
 document.addEventListener('DOMContentLoaded', () => {
     const projects = new ProjectManager();
     renderProjects(projects.getProjects());
 
-    //Adding Task Logic
     const taskDialog = document.getElementById('task-dialog');
     const addTaskBtn = addTaskButton();
 
     addTaskBtn.addEventListener('click', function () {
         taskDialog.showModal();
+    });
+    
+    const tasks = new TaskManager();
+    const taskForm = document.getElementById('task-form');
+
+    taskForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const title = document.getElementById('task-name').value;
+        const description = document.getElementById('description').value;
+        const priorityLevel = document.getElementById('priority-status').value;
+        const dueDate = 0;
+        tasks.createTask(title, description, priorityLevel, dueDate);
+        renderTask(tasks.getTasks());
+        taskDialog.close();
     })
 });
 
