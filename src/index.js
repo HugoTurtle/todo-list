ade44982ac230aa947ee3403c9777064bd0318c5
Hugo Perez <hugopereztm12309@gmail.com>
2025-06-12 17:43:31 -0400

    Render default project when DOM is loaded

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./styles.css";
+import { ProjectManager } from "./projectManager";
+import { renderProjects } from "./uiController";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const projects = new ProjectManager();
+    renderProjects(projects.getProjects());
+});
+
