fc4896d0ac08ecd211eff023ddc466cb535b1acb
Hugo Perez <hugopereztm12309@gmail.com>
2025-06-12 17:48:08 -0400

    Render add task button and attach event listener

--- src/index.js
+++ src/index.js
@@ -5,5 +5,13 @@ import { renderProjects } from "./uiController";
 document.addEventListener('DOMContentLoaded', () => {
     const projects = new ProjectManager();
     renderProjects(projects.getProjects());
+
+    //Adding Task Logic
+    const taskDialog = document.getElementById('task-dialog');
+    const addTaskBtn = addTaskButton();
+
+    addTaskBtn.addEventListener('click', function () {
+        taskDialog.showModal();
+    })
 });
 
