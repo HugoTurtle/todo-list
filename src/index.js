6968d2d959fdb6ffe878c308b5bfa0ff6c4ddc82
Hugo Perez <hugopereztm12309@gmail.com>
2025-06-15 19:31:59 -0400

    Format date

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { renderProjects } from "./uiController";
 import { addTaskButton } from "./uiController";
 import { TaskManager } from "./taskManager";
 import { renderTask } from "./uiController";
+import { parse, format, parseISO } from "date-fns";
 
 document.addEventListener('DOMContentLoaded', () => {
     const projects = new ProjectManager();
@@ -24,8 +25,12 @@ document.addEventListener('DOMContentLoaded', () => {
         const title = document.getElementById('task-name').value;
         const description = document.getElementById('description').value;
         const priorityLevel = document.getElementById('priority-status').value;
+
         const dueDate = document.getElementById('date-picker').value;
-        tasks.createTask(title, description, priorityLevel, dueDate);
+        const parsedDate = parseISO(dueDate);
+        const formattedDate = format((parsedDate), "MM/dd/yyyy");
+        
+        tasks.createTask(title, description, priorityLevel, formattedDate);
         renderTask(tasks.getTasks());
         taskDialog.close();
     })
