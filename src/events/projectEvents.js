aa22f64b57790e4b7ea614b5cf43bf638137eecd
Hugo Perez <hugopereztm12309@gmail.com>
2025-07-09 18:26:52 -0400

    Add project deletion logic

--- src/events/projectEvents.js
+++ src/events/projectEvents.js
@@ -4,6 +4,8 @@ export function initProjectEvents(projects, renderProjects, updateProjectOptions
     const addProjectButton = document.getElementById('add-project-btn');
     const projectDialog = document.getElementById('dialog');
     const projectForm = document.getElementById('dialog-form');
+
+    const projectContainer = document.getElementById('project-container');
     
     const allProjects = projects.getProjects();
 
@@ -26,6 +28,20 @@ export function initProjectEvents(projects, renderProjects, updateProjectOptions
         projectDialog.close();
         projectForm.reset();
 
+    })
+
+    //Handle project deletion
+    projectContainer.addEventListener('click', (e) => {
+        if(!e.target.matches('.delete-button')) return;
+
+        const projectElement = e.target.closest('.project-tab');
+        const projectIndex = Number(projectElement.getAttribute('data-project-id'));
+        console.log(projectIndex);
+        projects.deleteProject(projectIndex);
+                                                                                              
+        renderProjects(allProjects);
+        updateProjectOptions(allProjects);
+        updateProjectOptions(allProjects, 'edit-project-select');
     })
 
     //Cancel button
