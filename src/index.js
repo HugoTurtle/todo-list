4eb6b44751962f8dfae168a74e8f6bc0f271c9c8
Hugo Perez <hugopereztm12309@gmail.com>
2025-07-15 18:43:44 -0400

    Load tasks and projects from local storage

--- src/index.js
+++ src/index.js
@@ -12,11 +12,22 @@ import { addTaskButton } from "./uiController";
 import { initProjectEvents } from "./events/projectEvents";
 import { initStateHandler } from "./events/stateEvents";
 import { initTaskEvents } from "./events/taskEvents";
+import { 
+    loadProjects,
+    loadTasks, 
+} from "./storage";
 
 document.addEventListener('DOMContentLoaded', () => {
     //Initialize data managers
     const projects = new ProjectManager();
     const tasks = new TaskManager();
+
+    //Check local storage
+    const savedTasks = loadTasks();
+    const savedProjects = loadProjects();
+
+    tasks.loadTasksFromStorage(savedTasks);
+    projects.loadProjectsFromStorage(savedProjects);
     
     //Initial render
     renderProjects(projects.getProjects());
