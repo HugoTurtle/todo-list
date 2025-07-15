1081e22189d1228946d336dd34e5489c722ab9bb
Hugo Perez <hugopereztm12309@gmail.com>
2025-07-15 17:53:06 -0400

    Rename variables for better clarity

--- src/storage.js
+++ src/storage.js
@@ -3,16 +3,16 @@ const STORAGE_KEYS = {
     TASKS: 'todo-tasks',
 }
 
-export function saveProject(project) {
-    localStorage.setItem(STORAGE_KEYS.PROJECTS, JSON.stringify(project));
+export function saveProjects(projectArray) {
+    localStorage.setItem(STORAGE_KEYS.PROJECTS, JSON.stringify(projectArray));
 }
 
 export function loadProjects() {
     return JSON.parse(localStorage.getItem(STORAGE_KEYS.PROJECTS));
 }   
 
-export function saveTask(task) {
-    localStorage.setItem(STORAGE_KEYS.TASKS, JSON.stringify(task));
+export function saveTasks(taskArray) {
+    localStorage.setItem(STORAGE_KEYS.TASKS, JSON.stringify(taskArray));
 }
 
 export function loadTasks() {
